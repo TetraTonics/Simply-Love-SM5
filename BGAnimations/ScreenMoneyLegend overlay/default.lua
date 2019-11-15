d53af85040b8444fdf3f5b94c531c7a5df641ec3
Lamar Cooley <30600688+CrashCringle12@users.noreply.github.com>
2019-11-15 00:51:02 -0500

    Added MoneyLegend attract screen (price assist)
    Lists prices for cabby
    Will be improved in future this is just a mock for initial testing

---
+++ BGAnimations/ScreenMoneyLegend overlay/default.lua
@@ -0,0 +1,30 @@
+return Def.ActorFrame{
+	InitCommand=function(self) self:Center() end,
+
+	LoadActor("quarter.png")..{
+		InitCommand=function(self) self:shadowlength(1):y(60) end,
+		OnCommand=function(self) self:zoom(0.4):glow(1,1,1,1):glowshift():diffusealpha(0):sleep(3):decelerate(2):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
+	},
+	LoadFont("_wendy white")..{
+		Text=ScreenString("Header"),
+		InitCommand=function(self) self:shadowlength(1):y(-120):diffusealpha(0) end,
+		OnCommand=function(self) self:sleep(1.0):decelerate(1):diffusealpha(1):sleep(4):linear(0.75):diffusealpha(0) end,
+	},
+	LoadFont("Common Normal")..{
+		Text=ScreenString("Top"),
+		InitCommand=function(self) self:shadowlength(1):y(0):diffusealpha(0) end,
+		OnCommand=function(self) self:sleep(2.0):decelerate(1):diffusealpha(1):sleep(4):linear(0.75):diffusealpha(0) end,
+	},
+
+    LoadFont("Common Normal")..{
+		Text=ScreenString("Middle"),
+		InitCommand=function(self) self:shadowlength(1):y(120):diffusealpha(0) end,
+		OnCommand=function(self) self:sleep(3.0):decelerate(1):diffusealpha(1):sleep(4):linear(0.75):diffusealpha(0) end,
+    },
+    
+	LoadFont("Common Normal")..{
+		Text=ScreenString("Bottom"),
+		InitCommand=function(self) self:shadowlength(1):y(180):diffusealpha(0) end,
+		OnCommand=function(self) self:sleep(4.0):decelerate(1):diffusealpha(1):sleep(4):linear(0.75):diffusealpha(0) end,
+	}
+}
