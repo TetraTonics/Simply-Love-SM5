1ee135fadb50a6643184ed990f378023a577f578
Lamar Cooley <30600688+CrashCringle12@users.noreply.github.com>
2020-08-23 18:37:30 -0400

    Simply Corona Touch Up

--- BGAnimations/ScreenCoronaLines overlay/default.lua
+++ BGAnimations/ScreenCoronaLines overlay/default.lua
@@ -1,6 +1,4 @@
 return Def.ActorFrame{
-	InitCommand=function(self) end,
-
 	LoadActor("Guidelines.png")..{
 		InitCommand=function(self) self:FullScreen() end,
 		OnCommand=function(self) self:FullScreen():diffusealpha(0):decelerate(2):diffusealpha(1):sleep(4):linear(0.75):diffusealpha(0) end,
--- BGAnimations/ScreenMoneyLegend overlay/default.lua
+++ BGAnimations/ScreenMoneyLegend overlay/default.lua
@@ -2,20 +2,19 @@ return Def.ActorFrame{
 	InitCommand=function(self) self:Center() end,
 
 	LoadActor("quarter.png")..{
-		InitCommand=function(self) self:shadowlength(1):y(60) end,
-		OnCommand=function(self) self:zoom(0.4):glow(1,1,1,1):glowshift():diffusealpha(0):sleep(3):decelerate(2):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
+		InitCommand=function(self) self:shadowlength(1):y(-75) end,
+		OnCommand=function(self) self:zoom(0.7):glow(1,1,1,1):glowshift():diffusealpha(0):sleep(3):decelerate(2):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
 	},
 	LoadFont("Common Header")..{
 		Text=ScreenString("Header"),
-		InitCommand=function(self) self:shadowlength(1):y(-120):diffusealpha(0) end,
+		InitCommand=function(self) self:shadowlength(1):y(-130):diffusealpha(0) end,
 		OnCommand=function(self) self:sleep(1.0):decelerate(1):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
 	},
 	LoadFont("_eurostile normal")..{
 		Text=ScreenString("Top"),
-		InitCommand=function(self) self:shadowlength(1):y(0):diffusealpha(0) end,
+		InitCommand=function(self) self:shadowlength(1):y(80):diffusealpha(0) end,
 		OnCommand=function(self) self:sleep(2.0):decelerate(1):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
 	},
-
     LoadFont("_eurostile normal")..{
 		Text=ScreenString("Middle"),
 		InitCommand=function(self) self:shadowlength(1):y(120):diffusealpha(0) end,
@@ -25,11 +24,11 @@ return Def.ActorFrame{
 	LoadFont("_eurostile normal")..{
 		Text=ScreenString("Bottom"),
 		InitCommand=function(self) self:shadowlength(1):y(160):diffusealpha(0) end,
-		OnCommand=function(self) self:sleep(12.0):decelerate(1):diffusealpha(1):sleep(3):linear(0.75):diffusealpha(0) end,
+		OnCommand=function(self) self:sleep(3.0):decelerate(1):diffusealpha(1):sleep(8):linear(0.75):diffusealpha(0) end,
 	},
 	LoadFont("_eurostile normal")..{
 		Text=ScreenString("Start"),
 		InitCommand=function(self) self:shadowlength(1):y(200):diffusealpha(0) end,
-		OnCommand=function(self) self:sleep(4.0):decelerate(1):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
+		OnCommand=function(self) self:sleep(4.0):decelerate(1):diffusealpha(1):sleep(8):linear(0.75):diffusealpha(0) end,
 	}
 }
