373574d72bc72564acf80ab835126e94beeb581c
Lamar Cooley <30600688+CrashCringle12@users.noreply.github.com>
2020-09-09 15:15:33 -0400

    Preparing to pull latest beta changes
    In Preparation to begin patching the Corona Virus

--- BGAnimations/ScreenCoronaLines overlay/default.lua
+++ BGAnimations/ScreenCoronaLines overlay/default.lua
@@ -1,6 +1,7 @@
 return Def.ActorFrame{
+
 	LoadActor("Guidelines.png")..{
-		InitCommand=function(self) self:FullScreen() end,
-		OnCommand=function(self) self:FullScreen():diffusealpha(0):decelerate(2):diffusealpha(1):sleep(4):linear(0.75):diffusealpha(0) end,
+		InitCommand=function(self) end,
+		OnCommand=function(self) self:diffusealpha(0):decelerate(2):diffusealpha(1):sleep(4):linear(0.75):diffusealpha(0) end,
 	},
 }
