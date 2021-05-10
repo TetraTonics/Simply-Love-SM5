d18924966d75f5deab6478fd7042a0bc03e13a33
quietly-turning <quietly.turning@gmail.com>
2021-05-10 02:01:10 -0400

    new TitleMenu logos for para, kb7, techno
    kb7's logo is dynamically generated by overlaying the SelectColor icon
    for SL's current VisualStyle on top of a reusable kb7 asset.  This is
    structured as a directory like
    ./Graphics/_logos/kb7/default.lua
    
    and that^ returns an ActorFrame that handles its own sizing and
    positioning.
    
    This^ required me to slightly modify TitleMenu's Logo.lua to accommodate
    loading either a png or an ActorFrame from a directory.
    
    (cherry picked from commit 273d4ade1705fa349c066aea2712f364b65475e6)
    
    # Conflicts:
    #	BGAnimations/ScreenTitleMenu underlay/SimplySomething.lua

--- BGAnimations/ScreenTitleMenu underlay/SimplySomething.lua
+++ BGAnimations/ScreenTitleMenu underlay/SimplySomething.lua
@@ -1,44 +1,58 @@
 -- use the current game (dance, pump, etc.) to load the apporopriate logo
--- SL currently has logo assets for: dance, pump, techno
---   use the techno logo asset for less common games (para, kb7, etc.)
+
 local game = GAMESTATE:GetCurrentGame():GetName()
-if game ~= "dance" and game ~= "pump" then
-	game = "techno"
-end
+local path = ("/%s/Graphics/_logos/%s"):format( THEME:GetCurrentThemeDirectory(), game)
 
-
--- -----------------------------------------------------------------------
-local style = ThemePrefs.Get("VisualStyle")
-local image = "TitleMenu"
-
--- see: watch?v=wxBO6KX9qTA etc.
-if FILEMAN:DoesFileExist("/Themes/"..THEME:GetCurThemeName().."/Graphics/_VisualStyles/"..ThemePrefs.Get("VisualStyle").."/TitleMenuAlt (doubleres).png") then
-	if math.random(1,100) <= 10 then image="TitleMenuAlt" end
+-- Fall back on using the dance logo asset if one isn't found for the current game.
+-- We can't use FILEMAN:DoesFileExist() here because it needs an already-resolved path,
+-- but "path" (above) might resolve to either a png or a directory.
+--
+-- So, use ActorUtil.ResolvePath() to attempt to resolve the path.
+-- If it doesn't resolve to a valid StepMania path, it will return nil,
+-- and we can use that mean that neither a png nor a directory was found for this game.
+local resolved_path = ActorUtil.ResolvePath(path, 1, true)
+if resolved_path == nil then
+	game = "dance"
+	resolved_path = ("/%s/Graphics/_logos/dance.png"):format( THEME:GetCurrentThemeDirectory() )
 end
 
 -- -----------------------------------------------------------------------
 local af = Def.ActorFrame{}
 
 -- SIMPLY [something]
-af[#af+1] = LoadActor(THEME:GetPathG("", "_VisualStyles/"..style.."/"..image.." (doubleres).png"))..{
+af[#af+1] = Def.Sprite{
 	Name="Simply Text",
 	InitCommand=function(self)
+		self:playcommand("LoadImage")
+	end,
+	OffCommand=function(self) self:linear(0.5):shadowlength(0) end,
+	VisualStyleSelectedMessageCommand=function(self)
+		self:playcommand("LoadImage")
+	end,
+	LoadImageCommand=function(self)
 		if (style == "PSU") then
 			self:x(2):zoom(0.8):shadowlength(0.75)
 			self:y(-47)
 		else
+			local style = ThemePrefs.Get("VisualStyle")
+			local image = THEME:GetPathG("", "_VisualStyles/"..style.."/TitleMenu (doubleres).png")
+			local imageAlt = "/Themes/"..THEME:GetCurThemeName().."/Graphics/_VisualStyles/"..style.."/TitleMenuAlt (doubleres).png"
+			if FILEMAN:DoesFileExist(imageAlt) and math.random(1,100) <= 10 then
+				self:Load(imageAlt)
+			else
+				self:Load(image)
+			end
 			self:zoom(0.7):vertalign(top)
 			self:y(-102):shadowlength(0.75)
 		end
-
 	end,
-	OffCommand=function(self) self:linear(0.5):shadowlength(0) end
 }
 
 -- decorative arrows
 af[#af+1] = LoadActor(THEME:GetPathG("", "_logos/" .. game))..{
 	InitCommand=function(self)
-		self:y(style == "PSU" and -11 or -16)
+		self:y(-16)
+		self:visible(ThemePrefs.Get("VisualStyle") ~= "SRPG5")
 
 		-- get a reference to the SIMPLY [something] graphic
 		-- it's rasterized text in the Wendy font like "SIMPLY LOVE" or "SIMPLY THONK" or etc.
@@ -52,7 +66,10 @@ af[#af+1] = LoadActor(THEME:GetPathG("", "_logos/" .. game))..{
 		-- so, get the horizontal zoom factor of these decorative arrows
 		-- and apply it to the y-axis as well to maintain proportions
 		self:zoomy( self:GetZoomX() )
-	end
+	end,
+	VisualStyleSelectedMessageCommand=function(self)
+		self:visible(ThemePrefs.Get("VisualStyle") ~= "SRPG5")
+	end,
 }
 
 return af
